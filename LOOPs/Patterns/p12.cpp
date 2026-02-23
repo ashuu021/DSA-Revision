f0bed4096b5c8d39f1d32dddfac3f80cb0c664ae
ashuu021 <ashuu023@gmail.com>
2026-02-23 19:53:35 +0530

    pattern 12 done

--- LOOPs/Patterns/p12.cpp
+++ LOOPs/Patterns/p12.cpp
@@ -3,18 +3,24 @@ using namespace std;
 
 int main() {
     int n =4;
-    for (int  i = n; i >=1 ; i--)
-    {
-        // spaces
-    for (int j=1;j<=n-i;j++){
-        cout<<" ";
+    for(int i =1 ; i<=n ;i++){
+        // stars
+        for (int  j = 1; j <= i; j++)
+        {
+            cout<<"*";
+        }
+         // stars
+        for (int  j = 1; j <= 2*(n-i); j++)
+        {
+            cout<<" ";
+        }
+         // stars
+        for (int  j = 1; j <= i; j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+        
     }
-    // stasrs 
-     for (int j=1;j<=2*i-1;j++){
-        cout<<"*";
-    }
-    cout<<endl;
-    }
-    
     return 0;
 }
