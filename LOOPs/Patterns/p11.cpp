7e583cd3e1acf65d669ae2b77444b301c858ec6a
ashuu021 <ashuu023@gmail.com>
2026-02-23 18:13:14 +0530

    pattern 11 done

---
+++ LOOPs/Patterns/p11.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n =4;
+    for (int  i = 0; i <= n ; i++)
+    {
+        // spaces
+    for (int j=1;j<=n-i;j++){
+        cout<<" ";
+    }
+    // stasrs 
+     for (int j=1;j<=2*i-1;j++){
+        cout<<"*";
+    }
+    cout<<endl;
+    }
+    for (int  i = n; i >=1 ; i--)
+    {
+        // spaces
+    for (int j=1;j<=n-i;j++){
+        cout<<" ";
+    }
+    // stasrs 
+     for (int j=1;j<=2*i-1;j++){
+        cout<<"*";
+    }
+    cout<<endl;
+    }
+    return 0;
+}
