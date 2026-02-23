0c51f7d0b7823e2ddd337508ceaef1a34eddd791
ashuu021 <ashuu023@gmail.com>
2026-02-23 20:11:46 +0530

    pattern 14 done

---
+++ LOOPs/Patterns/p14.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n= 4;
+    bool val=true;
+    for(int i =1;i<=n;i++){
+for(int j =1 ;j<=i;j++){
+    cout<<val<<" ";
+    val=!val;
+}
+cout<<endl;
+    }
+    return 0;
+}
