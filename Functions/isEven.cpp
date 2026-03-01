d2bfade85354889af65cd84a3d8c219028f2011f
ashuu021 <ashuu023@gmail.com>
2026-03-01 20:10:55 +0530

    DSA Progress: Functions/isEven.cpp

--- Functions/isEven.cpp
+++ Functions/isEven.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 using namespace std;
-
-int main() {
+bool isEven(int n ){
+    if (n%2==0)
+    {
+        return true;
+    }
+    else{
+        return false;
+    }
     
+}
+int main() {
+int even = isEven(49);
+if (even){
+    cout<<"Even";
+}else{
+    cout<<"Odd";
+}
     return 0;
 }
