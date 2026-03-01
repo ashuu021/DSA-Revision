ae153fbb62b01b8bc8985996e04fa1953407c879
ashuu021 <ashuu023@gmail.com>
2026-03-01 20:21:38 +0530

    DSA Progress: Functions/factorial.cpp

---
+++ Functions/factorial.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+long long factorial(int n){
+    if(n < 0) return -1;
+
+    long long fact = 1;
+    for(int i = 1; i <= n; i++){
+        fact *= i;
+    }
+    return fact;
+}
+
+int main() {
+    cout << factorial(0) << endl;
+    cout << factorial(1) << endl;
+    cout << factorial(10) << endl;
+    cout << factorial(20) << endl;
+}
