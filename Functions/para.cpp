c7735003394efb81d3235b490b0bb349faf0bb28
ashuu021 <ashuu023@gmail.com>
2026-03-01 20:04:10 +0530

    DSA Progress: Functions/para.cpp

--- Functions/para.cpp
+++ Functions/para.cpp
@@ -8,11 +8,17 @@ int diff(int a ,int b){
     int sum =a-b;
     return sum;
 }
+int pro(int a ,int b){
+    int sum =a*b;
+    return sum;
+}
 int main() {
     int s =sum(4,6);
     int d =diff(4,6);
+    int p =pro(4,6);
     
     cout<<"Sum is :"<<s<<endl;
     cout<<"Difference  is :"<<d<<endl;
+    cout <<"Product is : "<<p<<endl;
     return 0;
 }
