ac6ba2abefdb2fb26fed2945b69731715639159b
ashuu021 <ashuu023@gmail.com>
2026-03-03 19:26:49 +0530

    DSA Progress: Functions/allPrimes.cpp

---
+++ Functions/allPrimes.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+bool prime(int n ){
+    if(n==1){
+        return false;
+    }
+    for(int i=2;i*i<=n;i++){
+        if(n%i==0){
+            return false;
+        }
+
+    }
+    return true;
+}
+void allPrimes(int n ){
+for(int i =2;i<=n;i++){
+    if(prime(i)){
+        cout<<i<<" ";
+    }
+}
+cout<<endl;
+}
+int main() {
+    allPrimes(13);
+    return 0;
+}
