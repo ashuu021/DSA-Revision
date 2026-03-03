0a30f5dd9c216754ee77028b9e5ec80c38355498
ashuu021 <ashuu023@gmail.com>
2026-03-03 19:07:06 +0530

    DSA Progress: Functions/binomialCoff.cpp

---
+++ Functions/binomialCoff.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int factorial(int n){
+    int fact=1;
+    for(int i =2;i<=n;i++){
+        fact*=i;
+    }
+    return fact;
+}
+int binoCoff(int n ,int r){
+    int val1=factorial(n);
+    int val2=r;
+    int val3=n-r;
+    int result=val1/(val2*val3);
+return result;
+}
+int main() {
+  cout<<binoCoff(4,2);
+    return 0;
+}
