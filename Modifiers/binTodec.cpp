245741cd3829c762e49d374ca90d587bb665c77e
ashuu021 <ashuu023@gmail.com>
2026-03-04 18:59:22 +0530

    DSA Progress: Modifiers/binTodec.cpp,tempCodeRunnerFile.cpp

---
+++ Modifiers/binTodec.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+void binTodec(int bin){
+    int n= bin;
+    int dec=0;
+    int pow=1; //2^1
+    while(n>0){
+        int digit =n%10;
+        dec+=digit*pow;
+        n=n/10;
+        pow=pow*2;
+
+    };
+    cout<<dec<<endl;
+}
+int main() {
+binTodec(1010);
+    return 0;
+}
