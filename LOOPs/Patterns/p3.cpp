53dafe9f6e02282adb18bfaab78368e9ade719b7
ashuu021 <ashuu023@gmail.com>
2026-03-12 09:46:24 +0530

    update

--- LOOPs/Patterns/p3.cpp
+++ LOOPs/Patterns/p3.cpp
@@ -1,4 +1,22 @@
 
+// // #include <iostream>
+// // using namespace std;
+// // // * * * * 
+// // // * * * 
+// // // * * 
+// // // * 
+// // int main() {
+// //     int n=4;
+// //        for (int i = 1; i <= n; i++)
+// //     {
+// //         for (int j =1; j<=n-i+1;j++){
+// //             cout<<"* ";
+// //         }
+// //         cout<<endl;
+// //     }
+// //     return 0;
+// // }
+
 // #include <iostream>
 // using namespace std;
 // // * * * * 
@@ -16,21 +34,3 @@
 //     }
 //     return 0;
 // }
-
-#include <iostream>
-using namespace std;
-// * * * * 
-// * * * 
-// * * 
-// * 
-int main() {
-    int n=4;
-       for (int i = 1; i <= n; i++)
-    {
-        for (int j =1; j<=n-i+1;j++){
-            cout<<"* ";
-        }
-        cout<<endl;
-    }
-    return 0;
-}
