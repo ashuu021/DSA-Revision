9742eaca4ad89f5dd2d8c394efde1b25fa02d530
Anshu Rathore <ashuu023@gmail.com>
2026-03-27 21:28:26 +0530

    Uncomment main function for data type size output
    Uncommented the code to enable size output for various data types.

--- Modifiers/modifier.cpp
+++ Modifiers/modifier.cpp
@@ -1,3 +1,15 @@
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     cout<<sizeof(int)<<endl;
+//     cout<<sizeof(long int)<<endl;
+//     cout<<sizeof(long long int )<<endl;
+//     cout<<sizeof(double)<<endl;
+//     cout<<sizeof(short int )<<endl;
+//     // cout<<sizeof(<<endl);
+//     return 0;
+// }
 #include <iostream>
 using namespace std;
 
